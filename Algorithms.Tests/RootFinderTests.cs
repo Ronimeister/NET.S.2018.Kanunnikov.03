250a37a09dc9a469aca4deb1545994e430d6d2ea
Ronimeister <ronimeisterhs@gmail.com>
2018-06-30 20:54:18 +0300

    Find Nth-root task added

---
+++ Algorithms.Tests/RootFinderTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class RootFinderTests
+    {
+        [TestCase(1, 5, 0.000_001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.041_006_25, 4, 0.0001, 0.45)]
+        [TestCase(0.027_9936, 7, 0.0001, 0.6)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004_241_979, 9, 0.000_000_01, 0.545)]
+        [TestCase(0.0081, 4, 0.1, 0.3)]
+        public void FindRoot_IsCorrect(double number, int power, double accuracy, double expectedResult) =>
+            Assert.AreEqual(RootFinder.FindRoot(number, power, accuracy), expectedResult, accuracy);
+
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.001, -2, 0.0001)]
+        [TestCase(0.01, 2, -1)]
+        public void FindRoot_Throws_ArgumentException(double number, int power, double accuracy)
+            => Assert.Throws<ArgumentException>(() => RootFinder.FindRoot(number, power, accuracy));
+    }
+}
---
+++ Algorithms/RootFinder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Class that allows to find Nth power root of double number with some accuracy.
+    /// </summary>
+    public static class RootFinder
+    {
+        #region Public API
+        /// <summary>
+        /// Public API of the class that check input data for the mistakes and return needed value.
+        /// </summary>
+        /// <param name="number">Number, which root we need to find.</param>
+        /// <param name="power">Root degree.</param>
+        /// <param name="accuracy">Accuracy of measurement.</param>
+        /// <returns>Nth root in form of real number.</returns>
+        public static double FindRoot(double number, int power, double accuracy)
+        {
+            if (accuracy <= 0 || accuracy >= 1)
+            {
+                throw new ArgumentException($"{nameof(accuracy)} must contains value in range: (0, 1).");
+            }
+
+            if (power % 2 == 0 && number < 0)
+            {
+                throw new ArgumentException($"{nameof(number)} will get invalid value through the current operation.");
+            }
+
+            if (power <= 0)
+            {
+                throw new ArgumentException($"{nameof(power)} must be positive.");
+            }
+
+            if (power == 1)
+            {
+                return number;
+            }
+
+            return FindNthRoot(number, power, accuracy);
+        }
+        #endregion
+
+        #region Private API
+        /// <summary>
+        /// Method that finds Nth root of the number. Realization of FindRoot() method.
+        /// </summary>
+        /// <param name="number">Number, which root we need to find.</param>
+        /// <param name="power">Root degree.</param>
+        /// <param name="accuracy">Accuracy of measurement.</param>
+        /// <returns>Nth root in form of real number.</returns>
+        private static double FindNthRoot(double number, int power, double accuracy)
+        {
+            double current = 1;
+            double next = SetNewIterationValue(current, power, number);
+
+            while (Math.Abs(current - next) >= accuracy)
+            {
+                current = next;
+                next = SetNewIterationValue(current, power, number);
+            }
+
+            return next;
+        }
+
+        /// <summary>
+        /// Method that set new value for the Newton algorithm.
+        /// </summary>
+        /// <param name="current">Current value of needed root value.</param>
+        /// <param name="power">Root degree.</param>
+        /// <param name="accuracy">Accuracy of measurement.</param>
+        /// <returns>New value for the Newton algorithm.</returns>
+        private static double SetNewIterationValue(double current, int power, double number)
+        {
+            return (((power - 1) * current) + (number / Math.Pow(current, power - 1))) / power;
+        }
+        #endregion
+    }
+}
